b357b0dbc1f6078578c7b42c7f68e1a5c4fc3eae
nsk5720 <nsg5720@naver.com>
2022-06-27 14:49:23 +0900

    Hello

---
+++ Hello.java
@@ -0,0 +1,7 @@
+package sec01.exam01;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("hello, welcome to the java world!");
+	}
+}
---
+++ Hello2.java
@@ -0,0 +1,8 @@
+package sec01.exam01;
+
+public class Hello2 {
+	public static void main(String[] args) {
+		System.out.println("hello, welcome to the java world!");
+	}
+
+}
